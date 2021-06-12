3049514f6c9b75c6364d690b5f728a99bd959bad
amarinoglobal <81358493+amarinoglobal@users.noreply.github.com>
2021-06-12 11:00:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+canvasRacingCar.init();
+window.onload = () => {
+  document.getElementById('start-button').onclick = () => {
+    // canvasRacingCar.startGame(); Función que activa obstaculos.
+  };
+
+  function startGame() { }
+};
+
